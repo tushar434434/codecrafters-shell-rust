c82f069211a8802f0f155be4727c2a17c4e5f4bd
Tushar Kumar <tusharkumar1036@gmail.com>
2026-06-17 11:17:31 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,24 @@ use std::io::{self, Write};
 fn main() {
     loop{
     // TODO: Uncomment the code below to pass the first stage
-     print!("$ ");
+     print!("$ type");
      io::stdout().flush().unwrap();
      let mut command = String::new();
      io::stdin().read_line(&mut command).unwrap();
-     if command.trim() == "exit"{
+    /* if command.trim() == "exit"{
         break;
      }
      else if command.starts_with("echo ") {//
-    println!("{}", &command[5..].trim());
+    println!("{}", &command[5..].trim());//trim otherwise give error
 }
      else{
      println!("{}: command not found",command.trim());
+    }*/
+    if command.trim == "type" || "echo" || "echo" {
+        println!("{} is shell builtin",command.trim() );
+    }
+    else {
+        println!("{}: command not found",command.trim());
     }
     }
     }
